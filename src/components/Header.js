a8c2bc69e5e54b4de5f8e7a8e45cbdef3aebec4f
Naman <namanb454@gmail.com>
2023-12-09 17:55:49 +0530

    new commit title and description

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,7 @@ function Header() {
       <div className='lg:w-1/2 w-full lg:my-40 mb-[20%] mx-auto text-white text-center'>
         <h2 className='w-fit lg:mx-20 mx-auto flex lg:text-5xl text-4xl font-bold text-center md:mb-10 mb-14' id='signupHeading' >
           WE &nbsp;
-          <div className='font-bold text-[#6F94C4EB] tracking-widest' id='signupsubHeading'>
+          <h1 className='font-bold text-[#6F94C4EB] tracking-widest' id='signupsubHeading'>
 
             <Typewriter
               options={{
@@ -26,9 +26,9 @@ function Header() {
                   'DESIGN', 'DEVELOP', 'DELIVER'
                 ],
               }} />
-          </div>
+          </h1>
         </h2>
-        <h3 className='w-fit text-white lg:text-base text-xl lg:mx-10 mx-1 lg:my-2 my-5 tracking-widest'>Breathing life into Ideas, Crafting and Creating with Excellence.</h3>
+        <h2 className='w-fit text-white lg:text-base text-xl lg:mx-10 mx-1 lg:my-2 my-5 tracking-widest'>Breathing life into Ideas, Crafting and Creating with Excellence.</h2>
         <h3 className='w-full text-white lg:text-base text-base text-center tracking-widest font-bold'>Your gateway to <span className='text-[#b6984b]'>seamless solutions</span>.</h3>
       </div>
       <div className='mt-auto animate-bounce hover:animate-none mx-auto w-fit'>
